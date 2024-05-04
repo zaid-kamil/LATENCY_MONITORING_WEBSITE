14569580124f4dcfc5d9e8ccbe841e5faa2e813e
shilpi <shilpiguptabca@gmail.com>
2024-05-04 11:22:17 +0530

    tasks.py

--- config/settings.py
+++ config/settings.py
@@ -116,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -137,3 +137,9 @@ MEDIA_ROOT = BASE_DIR.joinpath('media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# celery configuration
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+
+# celery timezone
+CELERY_TIMEZONE = 'Asia/Kolkata'
